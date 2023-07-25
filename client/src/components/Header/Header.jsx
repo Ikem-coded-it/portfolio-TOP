e176f231f18fdea1c9d216ae41145be4cca290b0
Ikem-coded-it <105087922+Ikem-coded-it@users.noreply.github.com>
2023-07-25 14:12:45 +0100

    finished desktop header

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import profilePic from "../../assets/images/IMG_20230305_080744_514~4.jpg";
+import "./styles.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="background"></div>
+      <div className="pic-about-container">
+        <img 
+          src={profilePic}
+          alt="ikem"
+          className="profile-pic"
+        />
+        <div className="about">
+          <h1>About Me</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi delectus iusto perferendis voluptate eius nesciunt exercitationem velit placeat eum consequatur, sit architecto similique illum fugit quisquam magnam? Facere sapiente beatae nemo sed a nihil possimus cum error explicabo numquam est deserunt velit, veritatis impedit. Atque error vel fuga eius iste?
+          </p>
+          <div className="link-icons-container">
+            <a rel="noreferrer" target="_blank" href="https://github.com/ikem-coded-it">
+              <i className="devicon-github-original colored"></i>
+            </a>
+            <a rel="noreferrer" target="_blank" href="https://twitter.com/IkemO06934594">
+              <i className="devicon-twitter-original colored"></i>
+            </a>
+            <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/ikemefuna-onubogu-84914323a">
+              <i className="devicon-linkedin-plain colored"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
